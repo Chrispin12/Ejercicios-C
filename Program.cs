6887367ea728eec6f02253cdfb4debb4ffe83de3
Acer <Acer@DESKTOP-G97RM7O>
2022-11-13 22:40:37 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ejercicio_23
+{
+    public class program
+    {
+        public static void Main()
+        {
+            int numer;
+            int resultado;
+            int doblado;
+
+            Console.WriteLine("Por favor ingrese un número"); 
+            
+            numer = Convert.ToInt32(Console.ReadLine()); 
+            
+            doblado= numer * 2;
+
+            resultado = doblado * 25;
+
+            Console.WriteLine($" El resultado de la operacion es: {resultado}");
+        }
+    }
+}
